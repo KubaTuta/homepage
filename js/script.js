e939c60a5adb3d9294f07d9b94903efc1a6e97c3
Kuba Tuta <tuta.jakub@gmail.com>
2022-12-22 11:47:05 +0100

    js refactoring

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
-let button = document.querySelector(".js-nav__button--motive");
-let body = document.querySelector(".js-body");
-let headerElements = document.querySelector(".js-header")
-let navElements = document.querySelector(".js-nav");
-let roadmapElements = document.querySelector(".js-roadmap");
-let aboutElements = document.querySelector(".js-about");
-let themeName = document.querySelector(".js-themeName");
-let footer = document.querySelector(".js-footer");
-let buttonElements = document.querySelectorAll(".js-nav__button");
-let nextElements = document.querySelectorAll(".js-about__next");
-let right = document.querySelector(".js-about__next--right");
-let left = document.querySelector(".js-about__next--left");
-let img = document.querySelector(".img");
+{
+const button = document.querySelector(".js-nav__button--motive");
+const right = document.querySelector(".js-about__next--right");
+const left = document.querySelector(".js-about__next--left");
+const img = document.querySelector(".img");
 
-button.addEventListener("click", () => {
+const changeTheme = () => {
+    const body = document.querySelector(".js-body");
+    const headerElements = document.querySelector(".js-header")
+    const navElements = document.querySelector(".js-nav");
+    const roadmapElements = document.querySelector(".js-roadmap");
+    const aboutElements = document.querySelector(".js-about");
+    const footer = document.querySelector(".js-footer");
+    const themeName = document.querySelector(".js-themeName");
+    const buttonElements = document.querySelectorAll(".js-nav__button");
+    const nextElements = document.querySelectorAll(".js-about__next");
+
     body.classList.toggle("bodyLight");
     navElements.classList.toggle("navLight");
     headerElements.classList.toggle("headerLight");
@@ -28,21 +30,28 @@ button.addEventListener("click", () => {
     });
 
     themeName.innerText = body.classList.contains("bodyLight") ? themeName.innerText = "Ciemny" : "Jasny";
-});
+};
 
-left.addEventListener("click", () => {
+const previousPhoto = () => {
     img.classList.toggle("nextPhoto");
     const thatsNextPhoto = img.classList.contains("nextPhoto");
     img.src = thatsNextPhoto ?
-        "https://i.postimg.cc/LX7x64Zf/chorwacja.jpg" :
-        "https://i.postimg.cc/zDVMm6RK/chill.jpg";
-    
-});
+        "images/chorwacja.jpg" :
+        "images/chill.jpg";
 
-right.addEventListener("click", () => {
+};
+
+const furtherPhoto = () => {
     img.classList.toggle("nextPhoto");
     const thatsNextPhoto = img.classList.contains("nextPhoto");
     img.src = thatsNextPhoto ?
-        "https://i.postimg.cc/LX7x64Zf/chorwacja.jpg" :
-        "https://i.postimg.cc/zDVMm6RK/chill.jpg";
-});
+        "images/chorwacja.jpg" :
+        "images/chill.jpg";
+}
+
+button.addEventListener("click", changeTheme);
+
+left.addEventListener("click", previousPhoto);
+
+right.addEventListener("click", furtherPhoto);
+}
